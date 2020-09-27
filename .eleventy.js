97681c6d5cb4107590dd08c850ff027720ed51e9
Ida Franceen <ida@kolombiken.com>
2020-09-27 18:32:51 +0200

    Adds fix for redirecting old urls

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   // Aliases are in relation to the _includes folder
   eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
   eleventyConfig.addLayoutAlias('start', 'layouts/start.html');
+  eleventyConfig.addLayoutAlias('redirect', 'layouts/redirect.html');
 
   // Pass some assets right through
   eleventyConfig.addPassthroughCopy("./src/assets/images/");
---
+++ src/sv/code-of-conduct.md
@@ -0,0 +1,9 @@
+---
+layout: redirect
+permalink: /code-of-conduct/
+redirect: /en/code-of-conduct/
+---
+
+This file is only for redirecting to the proper url in case anyone uses an old link to get to the content.
+
+(No, this content will not print cause the redirect template doesn't have any content.)

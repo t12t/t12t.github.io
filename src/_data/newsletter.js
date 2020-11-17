8c8efdfd972efcf9444e311083a2e54c27d719c3
Ida Franceen <ida@kolombiken.com>
2020-11-17 19:51:52 +0100

    Preventing unnecessary automated testing errors

--- src/_data/newsletter.js
+++ src/_data/newsletter.js
@@ -2,11 +2,13 @@ module.exports = {
   sv: {
     legend: "Prenumerera på nyhetsbrevet",
     label: "Din email",
+    bot: "Fyll inte i detta",
     button: "Prenumerera"
   },
   en: {
     legend: "Subscribe to the newsletter",
     label: "Your email",
+    bot: "Don't fill in this field",
     button: "Subscribe"
   }
 };
